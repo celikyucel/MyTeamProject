18bb3146979426d699b738bf0f6570d1b9c1c42b
celikyucel <clkyucel3@gmail.com>
2020-10-15 15:30:07 +0200

    second line

--- src/test/java/HelloTeam.java
+++ src/test/java/HelloTeam.java
@@ -3,5 +3,7 @@ public class HelloTeam {
     public static void main(String[] args) {
 
         System.out.println("This is my first team project");
+
+        System.out.println("This is second line");
     }
 }
