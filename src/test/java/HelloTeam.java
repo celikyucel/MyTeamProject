4b0b05e46d5d3d4434396cd30074b2809d637bb9
ersanefe <ersan.yalcinkaya@gmail.com>
2020-10-15 15:28:35 +0200

    ersan changing

--- src/test/java/HelloTeam.java
+++ src/test/java/HelloTeam.java
@@ -3,5 +3,6 @@ public class HelloTeam {
     public static void main(String[] args) {
 
         System.out.println("This is my first team project");
+        System.out.println("Ersan PUSH & Pull");
     }
 }
